8d9e1094954357a319e27d5832cec271a4b9171f
RafiaZeeshan14 <fullstack786.dev01@gmail.com>
2024-09-01 14:06:31 +0500

    Scroll updated

--- components/Scroll.tsx
+++ components/Scroll.tsx
@@ -1,33 +1,31 @@
-import React from 'react'
+import React from "react";
 
 const Scroll: React.FC<{}> = () => {
   return (
     <div>
-      <div className="scroll-container " >
-      <div className="scroll-content">
-  <span className='mr-5'>ACCESSIBILITY</span>
-  <span className='mr-5'>✦</span>
-  <span className='mr-5'>USER FRIENDLY</span>
-  <span className='mr-5'>✦</span>
-  <span className='mr-5'>DESIGN THINKING</span>
-  <span className='mr-5'>✦</span>
-  <span className='mr-5'>EMPATHY</span>
-  <span className='mr-5'> ✦</span>
-  <span className='mr-5'>RESPONSIVE DESIGN</span>
-  <span className='mr-5'>✦</span>
-  <span className='mr-5'>USER SATISFACTION</span>
-  <span className='mr-5'>✦</span>
-  <span className='mr-5'>SEAMLESS EXPERIENCE</span>
-  <span className='mr-5'>✦</span>
-  <span className='mr-5'>MOBILE FRIENDLY</span>
-  <span className='mr-5'>✦</span>
-  <span className='mr-5'>PROBLEM SOLVING</span>
-</div>
-
-</div>
-
+      <div className="scroll-container ">
+        <div className="scroll-content">
+          <span className="mr-5">ACCESSIBILITY</span>
+          <span className="mr-5">✦</span>
+          <span className="mr-5">USER FRIENDLY</span>
+          <span className="mr-5">✦</span>
+          <span className="mr-5">DESIGN THINKING</span>
+          <span className="mr-5">✦</span>
+          <span className="mr-5">EMPATHY</span>
+          <span className="mr-5"> ✦</span>
+          <span className="mr-5">RESPONSIVE DESIGN</span>
+          <span className="mr-5">✦</span>
+          <span className="mr-5">USER SATISFACTION</span>
+          <span className="mr-5">✦</span>
+          <span className="mr-5">SEAMLESS EXPERIENCE</span>
+          <span className="mr-5">✦</span>
+          <span className="mr-5">MOBILE FRIENDLY</span>
+          <span className="mr-5">✦</span>
+          <span className="mr-5">PROBLEM SOLVING</span>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Scroll
+export default Scroll;
